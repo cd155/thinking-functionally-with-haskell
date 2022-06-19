840045be87c0a5b5d30022754d49608b357378dc
cd155 <github@donchen.page>
2022-06-19 15:28:23 -0400

    add ex F and G

--- TwoExprs/Exercises.hs
+++ TwoExprs/Exercises.hs
@@ -44,3 +44,37 @@ first' p xs | null xs = Nothing
             | p x = Just x
             | otherwise = first' p $ tail xs
             where x = head xs
+
+-- Exercise F
+exp' :: Integer -> Integer -> Integer
+exp' x n | n == 0 = 1
+         | n == 1 = x
+         | otherwise = x * exp' x (n-1)
+
+-- divide conquer technique
+exp'' :: Integer -> Integer -> Integer
+exp'' x n | n == 0 = 1
+          | n == 1 = x
+          | even n = evenMul
+          | otherwise = evenMul * x-- odd
+          where evenMul = exp'' x (n `div` 2) * exp'' x (n `div` 2)
+
+-- Exercise G
+
+type Date = (Int, Int, Int)
+
+showDate :: Date -> String
+showDate (d, m, y) = showDay d ++ " "++ month !! (m-1) ++ ", " ++ show y
+
+showDay :: Int -> String
+showDay d
+    | d > 32 = error "Max number of day is 31"
+    | otherwise = show d ++ suffix (d `mod` 10)
+
+suffix :: Int -> String
+suffix 1 = "st"
+suffix 2 = "ed"
+suffix 3 = "rd"
+suffix _ = "th"
+
+month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
