f90e6869d4e9d85513aa9d241922b931548a2fed
cd155 <github@donchen.page>
2022-06-26 20:23:47 -0400

    complete floor

--- ThreeNums/InClassNotes.hs
+++ ThreeNums/InClassNotes.hs
@@ -12,5 +12,14 @@ until' p f x = if p x then x else until' p f (f x)
     fromInteger make sure x (-1) in until' is Integer
     x is (-1) because we assume x < 0. Therefore, this is the first case we want to check
 -}
-floor :: (Ord a, Num a) => a -> Integer
-floor x = until' ((<=x) . fromInteger) (subtract 1) (-1)
+
+floor' :: (Ord a, Num a) => a -> Integer
+floor' x 
+    | x < 0 =  floorSmZero x
+    | otherwise = floorGtrZero x
+
+floorSmZero :: (Ord a, Num a) => a -> Integer
+floorSmZero x = until' ((<=x) . fromInteger) (subtract 1) (-1)
+
+floorGtrZero :: (Ord a, Num a) => a -> Integer
+floorGtrZero x = until' ((>=x) . fromInteger) (+ 1) 0
