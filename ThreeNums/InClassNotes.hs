56b61721c24e697fa32aa2ed5592d9b6bd3c17ae
cd155 <github@postmail.app>
2022-09-07 16:40:07 -0400

    complete 3.3

--- ThreeNums/InClassNotes.hs
+++ ThreeNums/InClassNotes.hs
@@ -23,3 +23,43 @@ floorSmZero x = until' ((<=x) . fromInteger) (subtract 1) (-1)
 
 floorGtrZero :: (Ord a, Num a) => a -> Integer
 floorGtrZero x = until' ((>=x) . fromInteger) (+ 1) 0
+
+-- leq: input less equal to x
+-- input is -1
+leq :: Integer -> Float -> Bool
+leq m x = fromInteger m <= x
+
+-- lt: x less than input
+-- input is 1
+lt :: Float -> Integer -> Bool
+lt x n = x < fromInteger n
+
+floor x = if x < 0
+        then until (`leq` x) (subtract 1) (-1)
+        else until (x `lt`) (+1) 1 - 1
+        where m `leq` x = fromInteger m <= x
+              x `lt` n = x < fromInteger n
+
+floor'''' :: Float -> Integer
+floor'''' x = fst (until unit (shrink x) (bound x))
+    where unit (m,n) = m+1 == n
+
+type Interval = (Integer,Integer)
+shrink :: Float -> Interval -> Interval
+shrink x (m,n) = if p `leq` x then (p,n) else (m,p)
+    where p = choose (m,n)
+
+choose :: Interval -> Integer
+choose (m,n) = (m+n) `div` 2
+
+bound :: Float -> Interval
+bound x = (lower x, upper x)
+
+lower :: Float -> Integer
+lower x = until (`leq` x) (*2) (-1)
+
+upper :: Float -> Integer
+upper x = until (x `lt`) (*2) 1
+
+floor''''' x = if r < 0 then n-1 else n
+    where (n,r) = properFraction x
