57c31aef9e4f6bec27719ced0b55bfb381319d29
cd155 <github@postmail.app>
2022-09-08 15:50:46 -0400

    add natural number

--- ThreeNums/InClassNotes.hs
+++ ThreeNums/InClassNotes.hs
@@ -63,3 +63,38 @@ upper x = until (x `lt`) (*2) 1
 
 floor''''' x = if r < 0 then n-1 else n
     where (n,r) = properFraction x
+
+{-
+    3.4 Natural numbers 
+-}
+
+data Nat = Zero | Succ Nat deriving (Eq, Ord, Show)
+
+-- instance Eq Nat where
+--     Zero == Zero = True
+--     Zero == Succ n = False
+--     Succ m == Zero = False
+--     Succ m == Succ n = m == n
+
+-- instance Show Nat where
+--     show Zero = "Zero"
+--     show (Succ Zero) = "Succ Zero"
+--     show (Succ (Succ n)) = "Succ (" ++ show (Succ n) ++ ")"
+
+instance Num Nat where
+    m + Zero = m
+    m + Succ n = Succ (m+n)
+    m * Zero = Zero
+    m * (Succ n) = m * n + m
+    
+    abs n = n
+    signum Zero = Zero
+    signum (Succ n) = Succ Zero
+    
+    m - Zero = m
+    Zero - Succ n = Zero
+    Succ m - Succ n = m - n
+    
+    fromInteger x
+        | x <= 0 = Zero
+        | otherwise = Succ (fromInteger (x-1))
